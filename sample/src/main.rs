c46063e474ae656f025fa23a4ba5b219fef74414
sultan <sasha530000@gmail.com>
2023-06-30 07:07:23 +0300

    gg

--- sample/src/main.rs
+++ sample/src/main.rs
@@ -1,21 +1,32 @@
-#[derive(Debug)]
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
 
-struct Point<X1, Y1>{
-    x : X1,
-    y : Y1
-}
-impl <X1, Y1> Point<X1, Y1>{
-    fn mix<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2>{
-        Point{
-            x : self.x,
-            y : other.y
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
         }
     }
+
+    &s[..]
 }
 
 fn main() {
-    let a = Point{x: 1, y: 2.0};
-    let b = Point{x: "foma", y: 'g'};
-    let mix = a.mix(b);
-    println!("{:#?}", mix)
+    let my_string = String::from("hello world");
+
+    // `first_word` works on slices of `String`s, whether partial or whole
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+
+    // `first_word` works on slices of string literals, whether partial or whole
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
 }
